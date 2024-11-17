7a9a70c4a358a80e1fef88091c6ab3c1d094bfab
dsvasudev19 <vasudevds1729@gmail.com>
2024-11-17 11:30:09 +0530

    Updated Live URL

--- axiosIntance.ts
+++ axiosIntance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   // baseURL: 'http://localhost:3000/api',
-  baseURL: 'http://chatter.interactweb.agency/api',
+  baseURL: 'https://chatter.interactweb.agency/api',
   withCredentials: true
 })
 
