4a60374355651e9957aa9397659eaff2daf7d5f4
dsvasudev19 <vasudevds1729@gmail.com>
2025-03-18 08:12:32 +0530

    fixed socket connection

--- socket.ts
+++ socket.ts
@@ -1,10 +1,8 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://chatter.interactweb.agency"; 
-
+const SOCKET_URL = "wss://chatter.interactweb.agency/"; 
 const socket = io(SOCKET_URL, {
     withCredentials: true, 
-    transports: ["websocket"], 
 });
 
 export default socket;
